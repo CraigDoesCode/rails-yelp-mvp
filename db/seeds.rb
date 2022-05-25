72875fb48f77d601998686cb5b8458b2c872deea
craig norford <craig.a.norford@gmail.com>
2022-05-25 12:08:34 +0100

    restaurants controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+categories = %w[chinese italian japanese french belgian]
+puts 'clear db'
+Restaurant.destroy_all
+puts 'populate db(5)'
+
+5.times do
+  Restaurant.create!(name: Faker::Food.dish,
+                     address: Faker::Address.full_address,
+                     phone_number: Faker::PhoneNumber.phone_number,
+                     category: categories.sample)
+end
